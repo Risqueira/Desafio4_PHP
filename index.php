fdebd05815682d3856834caea9d13a8c188b96ff
Risqueira <henrique.mrodrigues77@gmail.com>
2026-06-15 17:28:07 -0300

    Finalização do index

--- index.php
+++ index.php
@@ -1,54 +1,67 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Page - Exercícios PHP</title>
     <link rel="stylesheet" href="styles/style.css">
-    <title>Document</title>
 </head>
 <body>
-    
-    // exemplos de PHP
-    <br><a href="exemplo1.php">Exemplo 1</a><br>
-    <a href="exemplo2.php">Exemplo 2</a><br>
-    <a href="exemplo3.php">Exemplo 3</a><br>
 
-    <br>
+<div class="container-index">
+    <h1>Exercícios de PHP</h1>
 
-    //Bloco 1: Algoritmos Sequenciais (Cálculos Simples)<br>
-    <a href="Bloco1/exe1.php">1 - Conversor de moedas</a><br>
-    <a href="Bloco1/exe2.php">2 - Calculadora de Área e Perímetro</a><br>
-    <a href="Bloco1/exe3.php">3 - Consumo de Combustível</a><br>
+    <!-- Bloco 1 -->
+    <div class="section">
+        <h2>Bloco 1: Algoritmos Sequenciais</h2>
+        <ul>
+            <li><a href="Bloco1/exe1.php">1 - Conversor de moedas</a></li>
+            <li><a href="Bloco1/exe2.php">2 - Calculadora de Área e Perímetro</a></li>
+            <li><a href="Bloco1/exe3.php">3 - Consumo de Combustível</a></li>
+        </ul>
+    </div>
 
-    <br><br>
+    <!-- Bloco 2 -->
+    <div class="section">
+        <h2>Bloco 2: Estruturas Condicionais (if/else)</h2>
+        <ul>
+            <li><a href="Bloco2/exe4.php">4 - Situação do Aluno (Média)</a></li>
+            <li><a href="Bloco2/exe5.php">5 - Verificador de Idade (Votação)</a></li>
+            <li><a href="Bloco2/exe6.php">6 - Par ou Ímpar</a></li>
+        </ul>
+    </div>
 
-    //Bloco 2: Estruturas Condicionais (if/else) <br>
-    <a href="Bloco2/exe4.php">4 - Situação do Aluno (Média)</a> <br>
-    <a href="Bloco2/exe5.php">5 - Verificador de Idade (Votação)</a> <br>
-    <a href="Bloco2/exe6.php">6 - Par ou Ímpar</a> 
+    <!-- Bloco 3 -->
+    <div class="section">
+        <h2>Bloco 3: Estruturas Condicionais (switch/case)</h2>
+        <ul>
+            <li><a href="Bloco3/exe7.php">7 - Dia da Semana</a></li>
+            <li><a href="Bloco3/exe8.php">8 - Operações Matemáticas</a></li>
+            <li><a href="Bloco3/exe9.php">9 - Mês por Extenso</a></li>
+        </ul>
+    </div>
 
-    <br><br>
+    <!-- Bloco 4 -->
+    <div class="section">
+        <h2>Bloco 4: Laços de Repetição</h2>
+        <ul>
+            <li><a href="Bloco4/exe10.php">10 - Fatorial de um Número</a></li>
+            <li><a href="Bloco4/exe11.php">11 - Somatório de 1 a N</a></li>
+            <li><a href="Bloco4/exe12.php">12 - Sequência de Pares</a></li>
+        </ul>
+    </div>
 
-    // Bloco 3: Estruturas Condicionais (switch/case) <br>
-    <a href="Bloco3/exe7.php">7 - Dia da Semana</a> <br>
-    <a href="Bloco3/exe8.php">8 - Operações Matemáticas (Calculadora Simples)</a> <br>
-    <a href="Bloco3/exe9.php">9 - Mês por Extenso</a>
+    <!-- Bloco 5 -->
+    <div class="section">
+        <h2>Bloco 5: Manipulação de Arrays</h2>
+        <ul>
+            <li><a href="Bloco5/exe13.php">13 - Média de Vários Valores</a></li>
+            <li><a href="Bloco5/exe14.php">14 - Lista de Compras</a></li>
+            <li><a href="Bloco5/exe15.php">15 - Encontrar o Maior Valor</a></li>
+        </ul>
+    </div>
 
-    <br><br>
-
-    //Bloco 4: Laços de Repetição (for ou while) <br>
-    <a href="Bloco4/exe10.php">10 - Fatorial de um Número</a> <br>
-    <a href="Bloco4/exe11.php">11 - Somatório de 1 a N</a> <br>
-    <a href="Bloco4/exe12.php">12 - Sequência de Pares (Intervalo)</a>
-
-    <br><br>
-
-    //Bloco 5: Manipulação de Arrays <br>
-    <a href="Bloco5/exe13.php">13 - Média de Vários Valores</a> <br>
-    <a href="Bloco5/exe14.php">14 - Lista de Compras (Checkboxes)</a> <br>
-    <a href="Bloco5/exe15.php">15 -  Encontrar o Maior Valor</a> <br>
-
-
+</div>
 
 </body>
 </html>
